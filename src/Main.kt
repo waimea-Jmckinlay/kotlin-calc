96746893bec332e335589e68b118bab9b8ba33c3
waimea-Jmckinlay <jcmckinlay@waimea.school.nz>
2025-02-24 11:01:07 +1300

    filters work need to make a answer fun

--- src/Main.kt
+++ src/Main.kt
@@ -4,37 +4,73 @@ fun main() {
     println(" kotlin calculator")
     println("---------------------------------------")
 
-    val firstnumber = getnumber ("Enter your first number")
+    val firstnumber = getInt ("Enter your first number")
 
-   val operator = getoperator ( "Enter operator(+ - * / ^ !)")
+   val operator = getOperator ( "Enter operator(+ - * / ^ !)")
 
-    val secondnumber = getnumber ("Enter your second number")
-
-
+    val secondnumber = getInt ("Enter your second number")
 
 
+    while (true){
+
+        if (operator == '+'){ println("$firstnumber + $secondnumber = $answer") break}
+
+       if (operator == '-'){println ("$firstnumber - $secondnumber = $answer") break}
+
+        if (operator == '*'){println ("$firstnumber * $secondnumber = $answer") break}
+
+        if (operator == '/'){println ("$firstnumber - $secondnumber = $answer") break}
 
 
 
-}
-
-
-// filtering so numbers only
-
-fun getnumber (prompt: String, ): Int {
-    var userinput: Int?
-    while (true) {
-        print(prompt)
-         userinput: = readln
-        if (userinput == null()) break
 
     }
 
-    return userinput !!
+
+
+
+
+
 
 }
+// filtering so numbers only
+//----------------------------------------------------------------------------------------------------------------
+fun getString(prompt: String): String {
+    var userinput: String
+    while (true) {
+        print(prompt)
 
-fun getoperator (prompt: String) {
-
-
+        userinput = readln()
+        if (userinput.isNotBlank()) break
+    }
+    return userinput
 }
+
+/**
+ * function to get an int from the user
+ */
+fun getInt(prompt: String): Int {
+    var intValue: Int?
+    while (true) {
+        val userinput: String = getString(prompt)
+        intValue = userinput.toIntOrNull()
+        if (intValue != null) break
+    }
+    return intValue!!
+}
+
+// filtering so they can only use the operators in the prompt
+//------------------------------------------------------------------------------------------------------
+
+fun getOperator (prompt: String): Char {
+    var op: Char
+    while (true) {
+        op = getString(prompt).first()
+
+        if ("+-*/^!".contains(op)) break
+
+    }
+    return op
+}
+
+
